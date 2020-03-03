b796069023973ce99405e656f33fbc34a0b3acae
lakshya97 <lakshya97@gmail.com>
2020-03-02 18:21:59 -0800

    making it even more general...

--- redner_adv.py
+++ redner_adv.py
@@ -6,6 +6,7 @@ import pyredner
 import matplotlib.pyplot as plt
 import urllib
 import zipfile
+#from imagenet_classes import class_names
 
 vgg16 = vgg.vgg16(pretrained=True)
 def set_grad(var):
@@ -15,9 +16,9 @@ def set_grad(var):
 
 
 class SemanticPerturbations:
-    def __init__(self, framework, filename):
+    def __init__(self, framework, filename, dims):
         self.framework = framework
-
+        self.image_dims = dims
         self.objects = pyredner.load_obj(filename, return_objects=True)
         self.camera = pyredner.automatic_camera_placement(self.objects, resolution=(512,512))
         vertices = []
@@ -28,9 +29,10 @@ class SemanticPerturbations:
         
         # Compute the center of the teapot
         self.center = torch.mean(torch.cat(vertices), 0)
-        self.translation = torch.tensor([10.0, -10.0, 10.0], device = pyredner.get_device(), requires_grad=True)
-        self.euler_angles = torch.tensor([0.1, -0.1, 0.1], device = pyredner.get_device(), requires_grad=True)
-
+        self.translation = torch.tensor([0., 0.0, 0.], device = pyredner.get_device(), requires_grad=True)
+        self.euler_angles = torch.tensor([0., 0., 0.], device = pyredner.get_device(), requires_grad=True)
+        self.light = pyredner.PointLight(position = (self.camera.position + torch.tensor((0.0, 0.0, 100.0))).to(pyredner.get_device()),
+                                                intensity = torch.tensor((20000.0, 30000.0, 20000.0), device = pyredner.get_device()))
     # image: the torch variable holding the image
     # net_out: the output of the framework on the image
     # label: the label of the image
@@ -58,15 +60,18 @@ class SemanticPerturbations:
         # Get the rotation matrix from Euler angles
         rotation_matrix = Variable(pyredner.gen_rotate_matrix(self.euler_angles), requires_grad=True)
         rotation_matrix.retain_grad()
+        print("Rotation")
+        print(rotation_matrix)
         # Shift the vertices to the center, apply rotation matrix,
         # shift back to the original space, then apply the translation.
         for obj in self.objects:
             obj.vertices = (obj.vertices - self.center) @ torch.t(rotation_matrix) + self.center + self.translation
             obj.vertices.retain_grad()
+            obj.normals = pyredner.compute_vertex_normal(obj.vertices, obj.indices)
         # Assemble the 3D scene.
         scene = pyredner.Scene(camera = self.camera, objects = self.objects)
         # Render the scene.
-        img = pyredner.render_albedo(scene)
+        img = pyredner.render_deferred(scene, lights=[self.light])
         img.retain_grad()
         return img
 
@@ -76,32 +81,32 @@ class SemanticPerturbations:
         # Visualize the initial guess
         eps = 1e-6
         img = torch.pow(img + eps, 1.0/2.2) # add .data to stop PyTorch from complaining
-        img = torch.nn.functional.interpolate(img.T.unsqueeze(0), size=(224,224), mode='bilinear')
+        img = torch.nn.functional.interpolate(img.T.unsqueeze(0), size=self.image_dims, mode='bilinear')
         img.retain_grad()
         return img
 
     def attack(self):
         # classify 
-        learning_rate = 1e-4
+        learning_rate = 10
         img = self.render_image()
         plt.imsave("out_images/base.png", img[0].T.data.cpu().numpy())
-        for i in range(10):
+        for i in range(25):
             pred, net_out = self.classify(img, 5)
             # get gradients
             self._get_gradients(img.cpu(), net_out, pred)
             for obj in self.objects:
+                #print(obj.vertices)
+                #print(obj.vertices.grad)
+                #print("*_*_*_*_*_*_*_*_*_*_*_*")
                 obj.vertices += obj.vertices.grad/torch.norm(obj.vertices.grad) * learning_rate
-            if self.translation.grad is None:
-                print("No translation grad")
-            else:
-                print("Hey!")
-                self.translation = self.translation + self.translation.grad/torch.norm(self.translation.grad) * learning_rate
-                self.translation.retain_grad()
+            #self.translation = self.translation + self.translation.grad/torch.norm(self.translation.grad) * learning_rate
+            #self.translation.retain_grad()
             img = self.render_image()
             plt.imsave("out_images/img_test_" + str(i) + ".png", img[0].T.data.cpu().numpy())
         final_pred, net_out = self.classify(img, 5)
         print(final_pred)
+        #print(class_names[final_pred])
         #plt.imsave("img_test.png", img[0].T.data.cpu().numpy())
 
-v = SemanticPerturbations(vgg16, "teapot/teapot.obj")
+v = SemanticPerturbations(vgg16, "teapot/teapot.obj", dims = (224,224))
 v.attack()
