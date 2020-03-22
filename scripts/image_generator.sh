7b88a85b407d619f529d122f78940e23ea268ed2
lakshya97 <lakshya97@gmail.com>
2020-03-22 02:41:56 -0700

    adding a script for image generation

---
+++ scripts/image_generator.sh
@@ -0,0 +1,4 @@
+python3 generate_images.py --id 02958343 --hashcode_file id_files/car_ids.txt
+python3 generate_images.py --id 03513137 --hashcode_file id_files/helmet_ids.txt
+python3 generate_images.py --id 03710193 --hashcode_file id_files/mailbox_ids.txt
+python3 generate_images.py --id 03790512 --hashcode_file id_files/motorcycle_ids.txt
