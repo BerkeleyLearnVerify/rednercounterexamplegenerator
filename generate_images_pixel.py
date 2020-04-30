cfe0d923e459ae4414c6416a28bdab5fe4f660c3
LAKSHYA JAIN <Lakshya.jain@berkeley.edu>
2020-04-30 02:46:55 -0700

    fixing CW pixel to be correct for fgsm/pgd

--- generate_images_pixel.py
+++ generate_images_pixel.py
@@ -1,49 +1,28 @@
-from redner_adv_pixel_cw import *
+from pixel_adv import *
 import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--id', type=str, help="the shapenet/imagenet ID of the class")
-parser.add_argument('--hashcode_file', type=str, help="a text file with a list of shapenet hashcodes")
+parser.add_argument('--img-dir', type=str, help="the relative path to the image directory")
+parser.add_argument('--img-list', type=str, help="a text file with all image names in the dir")
+parser.add_argument('--save-dir', type=str, help="the path to where you want to save the image")
 parser.add_argument('--label', type=int)
 parser.add_argument('--target', type=int)
-parser.add_argument('--pose', type=str, choices=['forward', 'top', 'left', 'right', 'all'], default='all')
 parser.add_argument('--attack', type=str, choices=['FGSM', 'PGD', 'CW'])
-parser.add_argument('--params', type=str, choices=["vertex", "pose", "all"], default="all")
 #for vgg16, shape is (224,224)
 
-
+ROOT_DIR = "/Users/lakshyajain/Desktop/rednercounterexamplegenerator/"
 args = parser.parse_args()
 
-hashcode_file = open(args.hashcode_file, 'r')
-hashcodes = []
-for line in hashcode_file.readlines():
-    hashcodes += [line.strip("\n")]
+image_file = open(args.img_list, 'r')
+image_names = []
+for line in image_file.readlines():
+    image_names += [line.strip("\n").split("/")[-1]]
 
-obj_id = args.id
 label = args.label
 target = args.target
 attack_type = args.attack
-
-pose = args.pose
-if pose == 'all':
-    poses = ['forward', 'top', 'left', 'right']
-else:
-    poses = [pose]
-
-vertex_attack = args.params == "vertex" or args.params == "all"
-pose_attack = args.params == "pose" or args.params == "all"
-
-print("Vertex Attack: ", vertex_attack)
-print("Pose Attack: ", pose_attack)
-
-background = "/work/yan.andy4/rednercounterexamplegenerator/lighting/blue_white.png"
-imagenet_filename = "/work/yan.andy4/rednercounterexamplegenerator/class_labels.json"
-
-if attack_type is None:
-    out_dir = "/work/yan.andy4/rednercounterexamplegenerator/out_pixel/benign/" + obj_id
-else:
-    out_dir = "/work/yan.andy4/rednercounterexamplegenerator/out_pixel/" + attack_type + "/" + args.params + "/" + obj_id
+out_dir = args.save_dir
 
 #background = "/home/lakshya/rednercounterexamplegenerator/lighting/blue_white.png"
 #imagenet_filename = "/home/lakshya/rednercounterexamplegenerator/class_labels.json"
@@ -59,39 +38,33 @@ vgg_params = {'mean': torch.tensor([0.6109, 0.7387, 0.7765]), 'std': torch.tenso
 
 total_errors = 0
 sample_size = 0
-for hashcode in hashcodes:
-    print(hashcode)
-    for pose in poses:
-        obj_filename = "/work/yan.andy4/rednercounterexamplegenerator/ShapeNetCore.v2/" + obj_id + "/" + hashcode + "/models/model_normalized.obj"
-        #out_dir += "/" + hashcode
-        try:
-            v = SemanticPerturbations(vgg16, obj_filename, dims=(224,224), label_names=get_label_names(imagenet_filename), normalize_params=vgg_params, background=background, pose=pose, attack_type=attack_type)
-            if attack_type is None:
-                v.render_image(out_dir=out_dir, filename=hashcode + '_' + pose + ".png")
-                print("\n\n\n")
-                continue
-            elif attack_type == "FGSM":
-                pred, img = v.attack_FGSM(label, out_dir=None, save_title=None, steps=5, vertex_eps=0.002, pose_eps=0.15,
-                                          vertex_attack=vertex_attack, pose_attack=pose_attack)
-                plt.imsave(out_dir + "/" + hashcode + '_' + pose + ".png", np.clip(img[0].permute(1, 2, 0).data.cpu().numpy(), 0, 1))
-            elif attack_type == "PGD":
-                pred, img = v.attack_PGD(label, out_dir=None, save_title=None, steps=5, vertex_epsilon=5.0, pose_epsilon=0.5, vertex_lr=0.01, pose_lr=0.20, vertex_attack=vertex_attack, pose_attack=pose_attack)
-                plt.imsave(out_dir + "/" + hashcode + '_' + pose + ".png", np.clip(img[0].permute(1, 2, 0).data.cpu().numpy(), 0, 1))
-            elif attack_type == "CW":
-                pred, img = v.attack_cw(label, out_dir=out_dir, save_title=hashcode + '_' + pose, steps=100, pixel_lr=3, target=target)
-                plt.imsave(out_dir + "/" + hashcode + '_' + pose + ".png", np.clip(img[0].permute(1, 2, 0).data.cpu().numpy(), 0, 1))
+v = PixelPerturb(framework=vgg16, framework_shape=(224,224), normalize_params=vgg_params)
+for image_name in image_names:
+    img = plt.imread(ROOT_DIR + args.img_dir + "/" + image_name)[:, :, :3]
+    pred, img = v.CW(img, iters=5, lr=0.01, label=label)
+    #out_dir += "/" + hashcode
+    try:
+        if attack_type == "FGSM":
+            pred, img = v.FGSM(img, iters=5, eps=0.01, label=label)
+            plt.imsave(ROOT_DIR + out_dir + "/" + image_name, np.clip(img, 0, 1))
+        elif attack_type == "PGD":
+            pred, img = v.PGD(img, iters=5, lr=0.1, epsilon=0.025, label=label)
+            plt.imsave(ROOT_DIR + out_dir + "/" + image_name, np.clip(img, 0, 1))
+        elif attack_type == "CW":
+            pred, img = v.CW(img, iters=5, lr=0.005, label=label)
+            plt.imsave(ROOT_DIR + out_dir + "/" + image_name, np.clip(img, 0, 1))
 
-            total_errors += (pred.item() != label)
-            sample_size += 1
-            print("Total Errors: ", total_errors)
-            print("Sample Size: ", sample_size)
-            print("\n\n\n")
+        total_errors += (pred != label)
+        sample_size += 1
+        print("Total Errors: ", total_errors)
+        print("Sample Size: ", sample_size)
+        print("\n\n\n")
 
-        except Exception as e:
-            print("ERROR")
-            print(e)
-            print("Error, skipping " + hashcode + ", pose " + pose)
-            continue
+    except Exception as e:
+        print("ERROR")
+        print(e)
+        print("Error, skipping " + image_name)
+        continue
 
 if attack_type is not None:
     print("Total number of misclassifications: ", total_errors)
--- pixel_adv.py
+++ pixel_adv.py
@@ -23,63 +23,64 @@ class PixelPerturb:
         self.framework_shape = framework_shape
         self.framework_params = normalize_params
         
-    def _get_gradients(net_out, label):
+    def _get_gradients(self, net_out, label):
         score = net_out[0][label]
-        score.backward(retain_graph=True)
+        score.backward()
 
     def classify(self, img):
         self.framework.eval()
         mean, std = self.framework_params["mean"], self.framework_params["std"]
         normalize = transforms.Normalize(mean, std)
-        image = normalize(image[0])
+        image = normalize(img[0])
         image = image.unsqueeze(0)
         # forward pass
-        fwd = self.framework.forward(image)
+        fwd = self.framework.forward(img)
         # classification via softmax
         probs, top5 = torch.topk(fwd, 5, 1, True, True)
         top5 = top5[0]
         probs = probs[0]
         pred = top5[0]
+        print("Prediction ", pred, "Probs ", probs)
         return fwd, pred
 
-    def FGSM(self, img, label, eps=0.01, iters=5, save=False, save_path=None):
-        assert img.shape == self.framework_shape
-        img = torch.tensor(img)
+    def FGSM(self, img, label, eps=0.000, iters=5, save=False, save_path=None):
+        img = torch.nn.functional.interpolate(torch.tensor(img).T.unsqueeze(0), size=self.framework_shape, mode='bilinear')
+        # img = torch.tensor(img)
         img = img.permute(0, 1, 3, 2)
         img = Variable(img, requires_grad=True)
-
+        img.retain_grad()
         optimizer = torch.optim.Adam([img], lr=0)
         for i in range(iters):
-            net_out, pred = classify(img)
+            net_out, pred = self.classify(img.clone())
             if pred.item() != label and i != 0:
-                img_return = img[0].permute(1, 2, 0).cpu().data
-                return img, pred.item()
+                img_return = img[0].permute(1, 2, 0).cpu().data.numpy()
+                return pred.item(), img_return
             self._get_gradients(net_out, label)
-            img -= eps * torch.sign(img.grad/torch.norm(img.grad))
+            img.data -= eps * torch.sign(img.grad)
             optimizer.zero_grad()
-        img_return = img[0].permute(1, 2, 0).cpu().data
-        return img.cpu().data, pred.item()
+        img_return = img[0].permute(1, 2, 0).cpu().data.numpy()
+        return pred.item(), img_return
 
     def PGD(self, img, label, lr=0.01, epsilon=0.5, iters=5, save=False, save_path=None):
-        assert img.shape == self.framework_shape
-        img = torch.tensor(img)
+        img = torch.nn.functional.interpolate(torch.tensor(img).T.unsqueeze(0), size=self.framework_shape, mode='bilinear')
+        # img = torch.tensor(img)
         img = img.permute(0, 1, 3, 2)
         img = Variable(img, requires_grad=True)
-
+        img.retain_grad()
         optimizer = torch.optim.Adam([img], lr=0)
         for i in range(iters):
-            net_out, pred = classify(img)
+            net_out, pred = self.classify(img.clone())
             if pred.item() != label and i != 0:
-                img_return = img[0].permute(1, 2, 0).cpu().data
-                return img, pred.item()
+                img_return = img[0].permute(1, 2, 0).cpu().data.numpy()
+                return pred.item(), img_return
             self._get_gradients(net_out, label)
-            img -= lr * torch.clamp(img.grad/torch.norm(img.grad), -epsilon, epsilon)
+            img.data -= torch.clamp(lr * img.grad, -epsilon, epsilon)
             optimizer.zero_grad()
 
-        img_return = img[0].permute(1, 2, 0).cpu().data
-        return img_return, pred.item()
+        img_return = img[0].permute(1, 2, 0).cpu().data.numpy()
+        return pred.item(), img_return
 
-    def _reduce_sum(x, keepdim=True):
+    def _reduce_sum(self, x, keepdim=True):
         # silly PyTorch, when will you get proper reducing sums/means?
         for a in reversed(range(1, x.dim())):
             x = x.sum(a, keepdim=keepdim)
@@ -87,77 +88,87 @@ class PixelPerturb:
         return x
 
 
-    def _l2_dist(x, y, keepdim=True):
+    def _l2_dist(self, x, y, keepdim=True):
         d = None
         for x_i, y_i in zip(x, y):
             if d is not None:
-                d += torch.sum(reduce_sum((x_i - y_i) ** 2, keepdim=keepdim))
+                d += torch.sum(self._reduce_sum((x_i - y_i) ** 2, keepdim=keepdim))
             else:
-                d = torch.sum(reduce_sum((x_i - y_i) ** 2, keepdim=keepdim))
+                d = torch.sum(self._reduce_sum((x_i - y_i) ** 2, keepdim=keepdim))
 
         return d
 
 
-    def _torch_arctanh(x, eps=1e-6):
-        x *= (1. - eps)
+    def _torch_arctanh(self, x, eps=1e-6):
+        x.data *= (1. - eps)
         return (torch.log((1 + x) / (1 - x))) * 0.5
 
 
-    def _tanh_rescale(x, x_min=-1., x_max=1.):
+    def _tanh_rescale(self, x, x_min=-1., x_max=1.):
         return (torch.tanh(x) + 1) * 0.5 * (x_max - x_min) + x_min
 
-    def _CW(self, img, label, target=None, lr=0.05):
-        img = torch.tensor(img)
+    def _cw_loss(self, output, target, dist, scale_const, targeted):
+        # compute the probability of the label class versus the maximum other
+        real = (target * output).sum(1)
+        other = ((1. - target) * output - target * 10000.).max(1)[0]
+        if targeted:
+            # if targeted, optimize for making the other class most likely
+            loss1 = torch.clamp(other - real, min=0.)  # equiv to max(..., 0.)
+        else:
+            # if non-targeted, optimize for making this class least likely.
+            loss1 = torch.clamp(real - other, min=0.)  # equiv to max(..., 0.)
+        loss1 = torch.sum(scale_const * loss1)
+
+        loss2 = dist.sum()
+        # print(loss1, loss2)
+        loss = loss1 + loss2
+        return loss
+
+    def CW(self, img, label, iters=5, target=None, lr=0.05):
+        img = torch.nn.functional.interpolate(torch.tensor(img).T.unsqueeze(0), size=self.framework_shape, mode='bilinear')
         img = img.permute(0, 1, 3, 2)
-        img = Variable(img, requires_grad=True)
+        orig_img = Variable(img, requires_grad=True)
+        orig_img.retain_grad()
 
         orig_img = img
-        img_modifier = torch.zeros(img.size(), device=pyredner.get_device(), requires_grad=True)
-        img = tanh_rescale(torch_arctanh(img) + self.img_modifier)
+        img_modifier = Variable(torch.zeros(img.size()), requires_grad=True)
+        img = self._tanh_rescale(self._torch_arctanh(orig_img) + img_modifier)
         modified_img = img
         
         if target is not None:
-            target = torch.tensor([target]).to(pyredner.get_device())
-            self.targeted = True
+            target = torch.tensor([target])
+            targeted = True
         else:
-            target = torch.tensor([label]).to(pyredner.get_device())
+            target = torch.tensor([label])
+            targeted = False
 
-        target_onehot = torch.zeros(target.size() + (NUM_CLASSES,)).to(pyredner.get_device())
+
+        target_onehot = torch.zeros(target.size() + (NUM_CLASSES,))
         target_onehot.scatter_(1, target.unsqueeze(1), 1.)
-        optimizer = torch.optim.Adam([img, img_modifier])
-        for i in range(steps):
+        optimizer = torch.optim.Adam([orig_img, img_modifier], lr=0)
+
+        for i in range(iters):
             optimizer.zero_grad()
-            pred, net_out = self.classify(img)
+            net_out, pred = self.classify(img.clone())
             if pred.item() != label and i != 0:
-                img_return = img[0].permute(1, 2, 0).cpu().data
+                img_return = img[0].permute(1, 2, 0).cpu().data.numpy()
                 return pred, img_return
 
             loss = 0
-            dist = l2_dist(input_adv_list, input_orig_list, False)
-            loss += self.cw_loss(net_out, target_onehot, dist, 0.1)
+            dist = self._l2_dist(orig_img, modified_img, False)
+            loss += self._cw_loss(net_out, target_onehot, dist, 0.1, targeted)
 
             loss.backward(retain_graph=True)
 
             delta = 1e-6
             inf_count = 0
             nan_count = 0
+            old_modifier = img_modifier.clone().detach()
+            img_modifier.data -= (img_modifier.grad/torch.norm(img_modifier.grad)) * lr
+            print(img_modifier)
+            img = self._tanh_rescale(self._torch_arctanh(orig_img) - old_modifier + img_modifier)
+            modified_img = img
 
-            # attack each shape's vertices
-            self.input_orig_list = []
-            self.input_adv_list = []
-
-            if not torch.isfinite(self.img_modifier.grad).all():
-                inf_count += 1
-            elif torch.isnan(self.img_modifier.grad).any():
-                nan_count += 1
-            else:
-                # subtract because we are trying to decrease the classification score of the label
-                old_modifier = img_modifier.clone().detach()
-                img_modifier.data -= (img_modifier.grad / (
-                    torch.norm(img_modifier.grad) + delta)) * lr
-
-                img = tanh_rescale(torch_arctanh(orig_img) - old_modifier + self.img_modifier)
-
-        final_pred, net_out = self.classify(img)
-        img_return = img[0].permute(1, 2, 0).cpu().data
-        return final_pred, img_return
+        net_out, pred = self.classify(img)
+        img_return = img[0].permute(1, 2, 0).cpu().data.numpy()
+        return pred.item(), img_return
