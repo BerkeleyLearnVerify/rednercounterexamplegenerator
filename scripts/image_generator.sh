97c626b2709a67d97827eea866ab6a0819c7e942
LAKSHYA JAIN <Lakshya.jain@berkeley.edu>
2020-05-10 23:28:12 -0700

    removing another useless folder

--- scripts/image_generator.sh
+++
@@ -1,4 +0,0 @@
-python3 generate_images.py --id 02958343 --hashcode_file id_files/car_ids.txt
-python3 generate_images.py --id 03513137 --hashcode_file id_files/helmet_ids.txt
-python3 generate_images.py --id 03710193 --hashcode_file id_files/mailbox_ids.txt
-python3 generate_images.py --id 03790512 --hashcode_file id_files/motorcycle_ids.txt
